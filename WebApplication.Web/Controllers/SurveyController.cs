d57e2c93c98139e00499ce2593f17ce220307c20
Allison Debeljak <and21@case.edu>
2019-03-14 10:00:38 -0400

    Added Survey controller and review survey results view

---
+++ WebApplication.Web/Controllers/SurveyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.DAL;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        private ISurveyDAO surveyDAO;
+        public SurveyController(ISurveyDAO surveyDAO)
+        {
+            this.surveyDAO = surveyDAO;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IList<Survey> results = surveyDAO.GetSurveyResults();
+            return View(results);
+        }
+
+        [HttpGet]
+        public IActionResult NewSurvey()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult NewSurvey(Survey survey)
+        {
+            surveyDAO.SaveSurvey(survey);
+            return RedirectToAction("Index");
+        }
+    }
+}
